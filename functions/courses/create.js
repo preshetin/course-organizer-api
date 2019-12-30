5c4196aff4d46f96e9406f1f67f92cef7306c661
Petr Reshetin <preshetin@gmail.com>
2019-12-30 16:23:42 +0300

    Initial commit

---
+++ functions/courses/create.js
@@ -0,0 +1,24 @@
+import uuid from "uuid";
+import * as dynamoDbLib from "../../libs/dynamodb-lib";
+import { success, failure } from "../../libs/response-lib";
+
+export async function main(event, context) {
+  const data = JSON.parse(event.body);
+  const params = {
+    TableName: process.env.coursesTableName,
+    Item: {
+      userId: event.requestContext.identity.cognitoIdentityId,
+      courseId: uuid.v1(),
+      description: data.description,
+      startDate: data.startDate,
+      createdAt: Date.now()
+    }
+  };
+
+  try {
+    await dynamoDbLib.call("put", params);
+    return success(params.Item);
+  } catch (e) {
+    return failure({ status: false });
+  }
+}
---
+++ functions/courses/update.js
@@ -0,0 +1,29 @@
+import * as dynamoDbLib from "../../libs/dynamodb-lib";
+import { success, failure } from "../../libs/response-lib";
+
+export async function main(event, context) {
+  const data = JSON.parse(event.body);
+  const params = {
+    TableName: process.env.coursesTableName,
+    Key: {
+      userId: event.requestContext.identity.cognitoIdentityId,
+      courseId: event.pathParameters.id
+    },
+    UpdateExpression: "SET description = :description, startDate = :startDate",
+    ExpressionAttributeValues: {
+      ":description": data.description || null,
+      ":startDate": data.startDate || null
+    },
+    // 'ReturnValues' specifies if and how to return the item's attributes,
+    // where ALL_NEW returns all attributes of the item after the update; you
+    // can inspect 'result' below to see how it works with different settings
+    ReturnValues: "ALL_NEW"
+  };
+
+  try {
+    await dynamoDbLib.call("update", params);
+    return success({ status: true });
+  } catch (e) {
+    return failure({ status: false });
+  }
+}
